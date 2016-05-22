3f282a05d821ba07448d2766fc85f4a277efa4cc
amansinghchauhan <iamanchauhan@gmail.com>
2016-05-23 03:03:04 +0530

    Update delete.php

--- include/delete.php
+++ include/delete.php
@@ -5,7 +5,7 @@ if(isset($_POST['todo_id']))
 	$todo_id = $_POST['todo_id'];
 	$sql="delete from todos where id=".$todo_id." and user='".$_SESSION['email']."'";
 	$result = mysqli_query($conn,$sql);//delete the item from the table
-	if(!$result)//if successfully inserted, execute below code.
+	if(!$result)//if the above code fails then run this
     {    
         //If mysql delete query was unsuccessful, output error 
         header('HTTP/1.1 500 Could not delete record!');//send the error record to the header
