89190cbad4857bbf7591a6e43421d5983520fad0
amansinghchauhan <iamanchauhan@gmail.com>
2016-05-23 02:13:33 +0530

    Change database details for heroku

--- include/init.php
+++ include/init.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 //Create Database Connection
-$conn= new mysqli('localhost', 'root', '','todolicious') or die('Could not connect: ' . mysql_error());
+$conn= new mysqli('ec2-54-243-201-19.compute-1.amazonaws.com', 'ldkcsswbmnnwbn', 'ymBcKZsY7fncQRfs3lwjQ7q2J-','d56f05eq9vi53g') or die('Could not connect: ' . mysql_error());
 ?>
